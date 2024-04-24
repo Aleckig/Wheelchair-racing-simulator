30e69f03c258bd77bfc4d0ea5bb466d9da3b75f7
ElBaron84 <99363250+ElBaron84@users.noreply.github.com>
2024-04-24 18:04:07 +0300

    Cleaning project

--- WheelchairRacingSimulator/Assets/Scripts/TestPathFollower.cs
+++ WheelchairRacingSimulator/Assets/Scripts/TestPathFollower.cs
@@ -6,8 +6,6 @@ public class TestPathFollower : MonoBehaviour
     public PathCreator pathCreator;
     public float speed = 0;
     private float distanceTravelled;
-    private bool speedingUp = false;
-    public float totalDistanceTraveled = 0f;
     private Vector3 lastPosition;
     public SpeedUpdater speedUpdater;
     public float speedMultiplier = 3.6f;
@@ -29,34 +27,6 @@ public class TestPathFollower : MonoBehaviour
         transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled);
         transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled);
 
-        // Handle input for speeding up
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            speedingUp = true;
-        }
-
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-            speedingUp = false;
-        }
-
-        // Adjust speed based on input
-        if (speedingUp)
-        {
-            speed += 0.3f;
-        }
-        else
-        {
-            speed -= 0.2f;
-            speed = Mathf.Max(speed, 0f);
-        }
-
-        // If there's actual movement, update the total distance traveled
-        if (speed != 0)
-        {
-            // Update the total distance traveled
-            totalDistanceTraveled += distanceThisFrame;
-        }
 
         // Set the last position to the current position for the next frame
         lastPosition = transform.position;
